99e1f4b7b77517f21f45caebc42bb5f63148f5b5
sel6606 <sel6606@g.rit.edu>
2017-11-14 16:33:21 -0500

    Created a countdown timer. The player is unable to type after the timer has run out

---
+++ Minigames/Assets/Scripts/Timer.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Script that handles the countdown timer for the game
+/// </summary>
+public class Timer : MonoBehaviour
+{
+    public float timeInSeconds;
+    public InputField input;
+    public Text timer;
+
+    private float timeRemaining;
+
+	// Use this for initialization
+	void Start ()
+    {
+        timeRemaining = timeInSeconds;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+        }
+        else
+        {
+            timeRemaining = 0;
+            GameOver();
+        }
+
+        timer.text = "Time Remaining: " + (int)timeRemaining;
+	}
+
+
+    /// <summary>
+    /// End the game
+    /// </summary>
+    void GameOver()
+    {
+        input.readOnly = true;
+        input.text = "";
+        input.DeactivateInputField();
+        input.interactable = false;
+    }
+}
