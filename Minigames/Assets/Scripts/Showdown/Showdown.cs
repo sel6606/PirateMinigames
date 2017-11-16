c3403bf620c2a443e929e585925abcab783688ba
Sean Mack <sjm3758@rit.edu>
2017-11-15 22:10:26 -0500

    added gamescreens for Showdown

--- Minigames/Assets/Scripts/Showdown/Showdown.cs
+++ Minigames/Assets/Scripts/Showdown/Showdown.cs
@@ -1,8 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Showdown : MonoBehaviour {
+
+    public GameObject instructionScreen;
+    public GameObject gameScreen;
+    public GameObject gameOver;
 
 	// Use this for initialization
 	void Start () {
