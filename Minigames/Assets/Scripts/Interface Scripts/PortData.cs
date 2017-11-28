1d09babb833ea63a59b0d43d71f642842cc924ab
sel6606 <sel6606@g.rit.edu>
2017-11-28 13:38:48 -0500

    Created the PortData and ShipData scripts.
    PortData is a struct that holds all of a port's data
    ShipData is a struct that holds all of a ship's data

---
+++ Minigames/Assets/Scripts/Interface Scripts/PortData.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Struct that stores all of a port's relevant data
+/// </summary>
+[System.Serializable]
+public struct PortData
+{
+    /// <summary>
+    /// The name of the port
+    /// </summary>
+    public string name;
+
+    /// <summary>
+    /// The amount of gold currently at the port
+    /// </summary>
+    public int goldAtPort;
+
+    /// <summary>
+    /// Which player controls the port (1 or 2).
+    /// If no player controls the port, the value is 0
+    /// </summary>
+    public int controllingPlayer;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="portName">The name of the port</param>
+    /// <param name="startingGold">The amount of gold the port starts with</param>
+    public PortData(string portName, int startingGold)
+    {
+        this.name = portName;
+        this.goldAtPort = startingGold;
+        this.controllingPlayer = 0;
+    }
+}
---
+++ Minigames/Assets/Scripts/Interface Scripts/ShipData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Struct that stores all of a ship's relevant data
+/// </summary>
+[System.Serializable]
+public struct ShipData
+{
+    public string name;
+    public int goldAmount;
+    public bool isFlagship;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="shipName">The name of the ship</param>
+    /// <param name="startingGold">The amount of gold the ship starts with</param>
+    /// <param name="isFlagship">Whether or not this ship is the flagship</param>
+    public ShipData(string shipName, int startingGold, bool isFlagship)
+    {
+        this.name = shipName;
+        this.goldAmount = startingGold;
+        this.isFlagship = isFlagship;
+    }
+}
