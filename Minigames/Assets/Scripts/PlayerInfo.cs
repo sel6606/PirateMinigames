76642d7033737aab5677c8eadb966937a72d7bc8
sel6606 <sel6606@g.rit.edu>
2017-11-28 13:52:50 -0500

    Created a PlayerInfo script that keeps track of the ship and port data between scenes

---
+++ Minigames/Assets/Scripts/PlayerInfo.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour
+{
+
+    //Represents the game info that is stored across all scenes
+    public static PlayerInfo instance;
+
+    #region Class Variables
+    private List<ShipData> shipsP1;
+    private List<ShipData> shipsP2;
+    private List<PortData> ports;
+    #endregion
+
+    #region Properties
+    public List<ShipData> ShipsP1
+    {
+        get { return shipsP1;}
+        set { shipsP1 = value; }
+    }
+
+    public List<ShipData> ShipsP2
+    {
+        get { return shipsP2; }
+        set { shipsP2 = value; }
+    }
+
+    public List<PortData> Ports
+    {
+        get { return ports; }
+        set { ports = value; }
+    }
+    #endregion
+
+    void Awake()
+    {
+        //If there is not already a PlayerInfo object, set it to this
+        if (instance == null)
+        {
+            //Object this is attached to will be preserved between scenes
+            DontDestroyOnLoad(gameObject);
+
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            //Ensures that there are no duplicate objects being made every time the scene is loaded
+            Destroy(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+        //Initialize either the default values or the values from the previous scenes
+        shipsP1 = instance.ShipsP1;
+        shipsP2 = instance.ShipsP2;
+        ports = instance.Ports;
+		
+	}
+
+}
