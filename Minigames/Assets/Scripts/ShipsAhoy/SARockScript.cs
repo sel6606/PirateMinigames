bf2691004211a426178915ea0bb43600128eb999
nac5961 <nac5961@g.rit.edu>
2017-12-02 19:28:58 -0500

    Created the rock script

--- Minigames/Assets/Scripts/ShipsAhoy/SARockScript.cs
+++ Minigames/Assets/Scripts/ShipsAhoy/SARockScript.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class SARockScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private float speed;
+
+    // Use this for initialization
+    void Start()
+    {
+        speed = 5.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    /// <summary>
+    /// Destroys the rock when it goes off screen
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Board Collider")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    /// <summary>
+    /// Moves coin down the screen
+    /// </summary>
+    private void Move()
+    {
+        transform.position += -transform.up * speed * Time.deltaTime;
+    }
 }
